07d89037d0a9a649afbd7c8d6b5b7d16ef5d350d
katherineyuneman <kyuneman@gmail.com>
2022-02-01 13:11:50 -0500

    Begin to create Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import NavBar from 'src/components/navigation/NavBar.js';
+import Home from './Home';
+import ProductsContainer from '../containers/ProductsContainer';
+import ProductCard from './products/ProductCard';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <NavBar />
+        <Switch>
+        <Route path ="/products/:id">
+            <ProductCard />
+          </Route>
+          <Route path ="/products">
+            <ProductsContainer />
+          </Route>
+          <Route path ="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
